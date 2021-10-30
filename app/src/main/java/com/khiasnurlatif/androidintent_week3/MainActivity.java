f2ccd17a89faaf659120d9f1bd87e760092654dc
Khias Nurlatif Ari Subekti <khiasnurlatifarisubekti@outlook.com>
2021-10-30 17:41:54 +0700

    Updating codes

--- app/src/main/java/com/khiasnurlatif/androidintent_week3/MainActivity.java
+++ app/src/main/java/com/khiasnurlatif/androidintent_week3/MainActivity.java
@@ -2,13 +2,20 @@ package com.khiasnurlatif.androidintent_week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
+
+    final public static String EXTRA_NAME = "extra_name";
+    final public static String EXTRA_EMAIL = "extra_email";
+    final public static String EXTRA_PHONE = "extra_phone";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +28,13 @@ public class MainActivity extends AppCompatActivity {
         EditText inputPassword = (EditText) findViewById(R.id.id_input_pwd);
 
         Button btnSave = (Button) findViewById(R.id.id_button_save);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Kode untuk aksi dari klik Button
-                Log.d("--Nama--", inputName.getText().toString());
-                Log.d("--Nama--", inputEmail.getText().toString());
-                Log.d("--Nama--", inputPhone.getText().toString());
-                Log.d("--Nama--", inputPassword.getText().toString());
-
-            }
+        btnSave.setOnClickListener(v ->{
+            Intent loginIntent = new Intent(MainActivity.this,
+                    DashboardActivity.class);
+            loginIntent.putExtra(EXTRA_NAME, inputName.getText().toString());
+            loginIntent.putExtra(EXTRA_EMAIL, inputEmail.getText().toString());
+            loginIntent.putExtra(EXTRA_PHONE, inputPhone.getText().toString());
+            startActivity(loginIntent);
         });
     }
 }
